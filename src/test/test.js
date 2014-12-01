fa2838039d5a48176b4ea5f67dc7c29c31a822f1
Sebastian Müller <balomueller@googlemail.com>
2014-12-01 13:07:20 -0500

    Basics

---
+++ src/test/test.js
@@ -0,0 +1,9 @@
+var expect = require("chai").expect;
+
+describe('Test', function(){
+  describe('first test', function(){
+    it('to be always true', function(){
+      expect(1).to.equal(1);
+    })
+  })
+})
