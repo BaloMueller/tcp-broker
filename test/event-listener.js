6d832d6b5618e4f48693c4edc3b34df80aca65eb
Sebastian Mueller <balomueller@googlemail.com>
2014-12-05 17:47:21 -0500

    Added more tests

--- test/event-listener.js
+++ test/event-listener.js
@@ -3,9 +3,6 @@ var expect = require('chai').expect,
     mockery = require('mockery');
 
 var sandbox = sinon.sandbox.create(),
-    stubbedClient = {
-        send: sandbox.stub()
-    },
     EventEmitter,
     eventEmitter,
     Listener,
@@ -40,15 +37,16 @@ describe('Listeners', function() {
 
   describe('broadcast event', function() {
     it('expected to be received by every listener', function(){
+    	var rawMsg = '0|B';
+    	var stubbedClient = { send: sandbox.spy().withArgs(rawMsg) };
       listener.clients[0] = stubbedClient;
 
       listener.HandleMsgFromSender({
 				'sequence': 0,
 				'type': 'B',
-				'msg': '0|B'
+				'raw': rawMsg
 			});
 
-			stubbedClient.send.yields();
 			expect(stubbedClient.send.calledOnce).to.be.true();
     });
   });
@@ -57,16 +55,19 @@ describe('Listeners', function() {
     it('expected follower to be added to this user', function(){
       listener.clients[0] = new Client(null);
 
+      var followers = listener.clients[0].getFollowers();
+			expect(followers).to.be.an('array');
+			expect(followers.length).to.equal(0);
+
       listener.HandleMsgFromSender({
 				'sequence': 0,
 				'type': 'F',
 				'from': 1,
 				'to': 0,
-				'msg': '0|F|0|1'
+				'raw': '0|F|0|1'
 			});
 
-      var followers = listener.clients[0].getFollowers();
-			expect(followers).to.be.an('array');
+      followers = listener.clients[0].getFollowers();
 			expect(followers.length).to.equal(1);
 			expect(followers[0]).to.equal(1);
     });
@@ -76,7 +77,7 @@ describe('Listeners', function() {
     it('expected follower to be added to this user', function(){
 			expect(Object.keys(listener.clients).length).to.equal(0);
       listener.clients[0] = new Client(null);
-      //listener.clients[0].addFollower(1);
+      listener.clients[0].addFollower(1);
 
       var followers = listener.clients[0].getFollowers();
 			expect(followers).to.be.an('array');
@@ -87,12 +88,50 @@ describe('Listeners', function() {
 				'type': 'U',
 				'from': 1,
 				'to': 0,
-				'msg': '0|U|0|1'
+				'raw': '0|U|1|0'
 			});
 
-      var followers = listener.clients[0].getFollowers();
-			expect(followers).to.be.an('array');
+      followers = listener.clients[0].getFollowers();
 			expect(followers.length).to.equal(0);
+    });
+  });
+
+  describe('private message event', function() {
+    it('expected message to be sent to the receipient', function(){
+    	var rawMsg = '0|P|0|1';
+    	var stubbedClient = { send: sandbox.spy().withArgs(rawMsg) };
+      listener.clients[0] = stubbedClient;
+
+      listener.HandleMsgFromSender({
+				'sequence': 0,
+				'type': 'P',
+				'from': 1,
+				'to': 0,
+				'raw': rawMsg
+			});
+
+			expect(stubbedClient.send.calledOnce).to.be.true();
+    });
+  });
+
+  describe('status update event', function() {
+    it('expected message to be sent to the receipient', function(){
+    	var rawMsg = '0|S|0|1';
+      
+      listener.clients[0] = new Client(null);
+      listener.clients[0].addFollower(1);
+    	
+    	var stubbedClient = { send: sandbox.spy().withArgs(rawMsg) };
+      listener.clients[1] = stubbedClient;
+
+      listener.HandleMsgFromSender({
+				'sequence': 0,
+				'type': 'S',
+				'from': 0,
+				'raw': rawMsg
+			});
+
+			expect(stubbedClient.send.calledOnce).to.be.true();
     });
   });
 });
